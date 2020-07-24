9d4203fdc68059e115d052521f1af9cc69a94581
Eric Mesa <ericsbinaryworld@gmail.com>
2020-07-23 20:56:25 -0400

    Solution to the Base Exercise
    I was only able to do this thanks to reading Trey's email about list comprehension which led to his web page about list comprehension. The combined with looking through my Python notes in Dokuwiki and coming across zip.

--- add/add.py
+++ add/add.py
@@ -1,16 +1,5 @@
 def add(matrix1, matrix2):
-    summed = [
-        val1 + val2
-        for list1, list2 in zip(matrix1, matrix2)
-        for val1, val2 in zip(list1, list2)
-    ]
-    print(summed)
-    final_list = []
-    for list1, list2 in zip(matrix1, matrix2):
-        final_list.append([val1 + val2 for val1, val2 in zip(list1, list2)])
-    print(final_list)
-
-    return list(zip(matrix1, matrix2))
+    return [[val1 + val2 for val1, val2 in zip(list1, list2)] for list1, list2 in zip(matrix1, matrix2)]
 
 
 if __name__ == "__main__":
