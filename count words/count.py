0e457f8350cd822c5bd6d2f282b2259f16fea153
Eric Mesa <ericsbinaryworld@gmail.com>
2020-08-11 19:20:22 -0400

    second attempt
    First attempt failed because it was counting letters, not words

--- count words/count.py
+++ count words/count.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 def count_words(phrase):
     counter = Counter()
-    for word in phrase:
+    sentence_list = phrase.split(" ")
+    for word in sentence_list:
         counter[word] += 1
     return counter
+
+# first attempt failed because it's counting letters, not words
