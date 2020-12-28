2230715d19d2195a56f5bcfd78e347f29cb40bc2
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-27 21:17:51 -0500

    Now I handle negatives

--- float_range/float_range.py
+++ float_range/float_range.py
@@ -9,9 +9,18 @@ class float_range:
 
     def __next__(self):
         next_number = self.start
-        while self.start < self.stop:
-            self.start += self.increment
-        return next_number
+        if self.increment > 0:
+            if self.start < self.stop:
+                self.start += self.increment
+                return next_number
+            else:
+                raise StopIteration
+        else:
+            if self.start > self.stop:
+                self.start += self.increment
+                return next_number
+            else:
+                raise StopIteration
 
     def __call__(self, start=0.0, stop=0.0, increment=1.0):
         self.start = start
@@ -21,4 +30,6 @@ class float_range:
 
 if __name__ == '__main__':
     for n in float_range(0.5, 2.5, 0.5):
+        print(n)
+    for n in float_range(2.5, 0, -0.5):
         print(n)
